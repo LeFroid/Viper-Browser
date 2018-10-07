66c39ed26a7094d73f36ecc183ac9719f1f8b06a
Timothy Vaccarelli <tmvfroid@gmail.com>
2018-10-07 09:47:18 -0400

    AutoFill: run on sub-frames when possible

--- src/JavaScript/AutoFill.js
+++ src/JavaScript/AutoFill.js
@@ -1,13 +1,26 @@
 (function() {
     var autoFillVals = {};
     %1
-    var inputElems = document.getElementsByTagName('input');
-    for (let e of inputElems) {
-        let eType = e.type.toLowerCase();
-        if (eType == 'email' || eType == 'password' || eType == 'text') {
-            if (e.name in autoFillVals) {
-                e.value = autoFillVals[e.name];
+
+    var fillInFor = function(doc) {
+        var inputElems = doc.getElementsByTagName('input');
+        for (let e of inputElems) {
+            let eType = e.type.toLowerCase();
+            if (eType == 'email' || eType == 'password' || eType == 'text') {
+                if (e.name in autoFillVals) {
+                    e.value = autoFillVals[e.name];
+                }
             }
+        }
+    };
+
+    fillInFor(document);
+    var subFrames = document.getElementsByTagName('iframe');
+    if (subFrames && subFrames.length > 0) {
+        for (var i = 0; i < subFrames.length; ++i) {
+            try {
+                fillInFor(subFrames[i].contentDocument);
+            } catch(ex) {}
         }
     }
 })();
