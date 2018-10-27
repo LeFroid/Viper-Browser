686e10f219f90820a557acd8f87bb3ed847ee2c0
Timothy Vaccarelli <tmvfroid@gmail.com>
2018-10-27 12:47:40 -0400

    Improve subframe support in WebChannelSetup script

--- src/JavaScript/WebChannelSetup.js
+++ src/JavaScript/WebChannelSetup.js
@@ -1,6 +1,15 @@
 (function() {
     %1
 
+    function notifySetupComplete() {
+        if (!window._webchannel_initialized) {
+            var event = document.createEvent('Event');
+            event.initEvent('_webchannel_setup', true, true);
+            window._webchannel_initialized = true;
+            document.dispatchEvent(event);
+        }
+    }
+
     function setupWebChannel() {
         new QWebChannel(qt.webChannelTransport, function(channel) {
             const viper = {};
@@ -8,12 +17,7 @@
             viper.favicons = channel.objects.favicons;
             viper.autofill = channel.objects.autofill;
             window.viper = viper; 
-            if (!window._webchannel_initialized) {
-                var event = document.createEvent('Event');
-                event.initEvent('_webchannel_setup', true, true);
-                window._webchannel_initialized = true;
-                document.dispatchEvent(event);
-            }
+            notifySetupComplete();
         });
     }
 
@@ -25,5 +29,17 @@
         }
     }
 
-    attemptSetup();
+    if (self !== top) {
+        if (top._webchannel_initialized) {
+            window.viper = top.viper;
+            notifySetupComplete();
+        } else {
+            top.document.addEventListener('__webchannel_setup', function() {
+                window.viper = top.viper;
+                notifySetupComplete();
+            });
+        }
+    } else {
+        attemptSetup();
+    }
 })();
