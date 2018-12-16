4a5ec6d21ed3795e0f3a4a21835e75f35858a8c3
Timothy Vaccarelli <tmvfroid@gmail.com>
2018-12-16 10:11:41 -0500

    Refactored some of the match pattern test code, mostly to learn about best practices that will be applied to new tests.

--- tests/regexp-test/tst_RegExpFilter.cpp
+++ tests/regexp-test/tst_RegExpFilter.cpp
@@ -32,6 +32,9 @@ private Q_SLOTS:
      */
     void testElementHideShouldNotMatchRegExp();
 
+    /// Generates the data used for testing the validity and correctness of various match patterns
+    void testMatchPatternMatches_data();
+
     /**
      * Tests the getRegExpForMatchPattern function with strings that should yield a positive match.
      */
@@ -55,29 +58,41 @@ void RegExpFilter::testValidityOfRegExp()
     QVERIFY2(m_elementRegExp.isValid(), "Regular expression for element hiding is invalid");
 }
 
+void RegExpFilter::testMatchPatternMatches_data()
+{
+    QTest::addColumn<QString>("original");
+    QTest::addColumn<QRegularExpression>("pattern");
+    QTest::addColumn<QString>("target");
+
+    QString googleMatchPattern = QStringLiteral("https://*.google.com/foo*bar");
+    QRegularExpression googlExpr = CommonUtil::getRegExpForMatchPattern(googleMatchPattern);
+    QTest::newRow("google doc foobar match") << googleMatchPattern << googlExpr << "https://docs.google.com/foobar";
+    QTest::newRow("google foo baz bar match") << googleMatchPattern << googlExpr << "https://www.google.com/foo/baz/bar"; 
+    
+    QString googleMailPattern = QStringLiteral("*://mail.google.com/*");
+    QRegularExpression googleMailExpr = CommonUtil::getRegExpForMatchPattern(googleMailPattern);
+    QTest::newRow("google mail match") << googleMailPattern << googleMailExpr << "https://mail.google.com/mail/u/0/";
+    QTest::newRow("google mail message match") << googleMailPattern << googleMailExpr << "https://mail.google.com/mail/u/0/#someMessage";
+    
+    QString localFilePattern = QStringLiteral("file:///foo*");
+    QRegularExpression localFileExpr = CommonUtil::getRegExpForMatchPattern(localFilePattern);
+    QTest::newRow("file match wildcard ending") << localFilePattern << localFileExpr << "file:///foo/bar.html";
+    QTest::newRow("file match exact") << localFilePattern << localFileExpr << "file:///foo";
+}
+
 void RegExpFilter::testMatchPatternMatches()
 {
-    QRegularExpression googlExpr = CommonUtil::getRegExpForMatchPattern(QStringLiteral("https://*.google.com/foo*bar"));
-    QString googleExprErrorMsg
-            = QString("Regular expression calculated is invalid. Original string: https://*.google.com/foo*bar , Regular Expression: %1").arg(googlExpr.pattern());
-    QVERIFY2(googlExpr.isValid(), googleExprErrorMsg.toStdString().c_str());
-    qDebug() << "Match pattern: https://*.google.com/foo*bar translated to " << googlExpr.pattern();
-    QVERIFY2(googlExpr.match(QStringLiteral("https://docs.google.com/foobar")).hasMatch(), "Pattern should match https://docs.google.com/foobar");
-    QVERIFY2(googlExpr.match(QStringLiteral("https://www.google.com/foo/baz/bar")).hasMatch(), "Pattern should match https://www.google.com/foo/baz/bar");
+    QFETCH(QString, original);
+    QFETCH(QRegularExpression, pattern);
+    QFETCH(QString, target);
 
-    QRegularExpression googleMailExpr = CommonUtil::getRegExpForMatchPattern(QStringLiteral("*://mail.google.com/*"));
-    QString googleMailExprErrorMsg
-            = QString("Regular expression calculated is invalid. Original string: *://mail.google.com/* , Regular Expression: %1").arg(googleMailExpr.pattern());
-    QVERIFY2(googleMailExpr.isValid(), googleMailExprErrorMsg.toStdString().c_str());
-    qDebug() << "Match pattern: *://mail.google.com/* translated to " << googleMailExpr.pattern();
+    QString validityQString = QString("Regular expression calculated from match pattern is invalid. Original string: %1, Derived Pattern: %2").arg(original).arg(pattern.pattern());
+    const char *validityErrMsg = validityQString.toStdString().c_str();
+    QVERIFY2(pattern.isValid(), validityErrMsg);
 
-    QRegularExpression localFileExpr = CommonUtil::getRegExpForMatchPattern(QStringLiteral("file:///foo*"));
-    QString localFileExprErrorMsg =
-            QString("Regular expression calculated is invalid. Original string: file:///foo* , Regular Expression: %1").arg(localFileExpr.pattern());
-    QVERIFY2(localFileExpr.isValid(), localFileExprErrorMsg.toStdString().c_str());
-    QVERIFY2(localFileExpr.match(QStringLiteral("file:///foo/bar.html")).hasMatch(), "Pattern should match file:///foo/bar.html");
-    QVERIFY2(localFileExpr.match(QStringLiteral("file:///foo")).hasMatch(), "Pattern should match file:///foo");
-    qDebug() << "Match pattern: file:///foo* translated into " << localFileExpr.pattern();
+    QString hasMatchQString = QString("Pattern %1 should match %2").arg(original).arg(target);
+    const char *hasMatchErrMsg = hasMatchQString.toStdString().c_str();
+    QVERIFY2(pattern.match(target).hasMatch(), hasMatchErrMsg);
 }
 
 void RegExpFilter::testMatchPatternBad()
