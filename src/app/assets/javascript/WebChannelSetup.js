345bb944c4c2f01020e3c01cb1789af4721d7382
Timothy Vaccarelli <tmvfroid@gmail.com>
2019-02-03 09:31:22 -0500

    Fix static analysis issue

--- src/app/assets/javascript/WebChannelSetup.js
+++ src/app/assets/javascript/WebChannelSetup.js
@@ -33,7 +33,7 @@
     function onMessage(event) {
         try {
             var msgData = event.data;
-            if (!('_viper_webchannel_msg' in msgData) || !msgData._viper_webchannel_msg)
+            if (msgData._viper_webchannel_msg !== true)
                 return;
 
             var channelObj = window.viper[msgData._viper_webchannel_obj];
