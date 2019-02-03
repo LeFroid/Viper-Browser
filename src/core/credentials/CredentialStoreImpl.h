1f7b192e3bdf0be289952f263ccf0c4ce9d82385
Timothy Vaccarelli <tmvfroid@gmail.com>
2019-02-03 16:17:29 -0500

    CredentialStoreImpl - rather than defining this as nullptr_t on unsupported systems, use an empty class definition

--- src/core/credentials/CredentialStoreImpl.h
+++ src/core/credentials/CredentialStoreImpl.h
@@ -8,7 +8,26 @@ using CredentialStoreImpl = CredentialStoreKWallet;
 
 #else
 
-using CredentialStoreImpl = std::nullptr_t;
+#include "CredentialStore.h"
+
+/// Empty class for unsupported systems
+class CredentialStoreImpl final : public CredentialStore
+{
+public:
+    CredentialStoreImpl() : CredentialStore() {}
+
+    ~CredentialStoreImpl() {}
+
+    std::vector<QString> getHostNames() override { return std::vector<QString>(); }
+
+    void addCredentials(const WebCredentials &credentials) override {}
+
+    std::vector<WebCredentials> getCredentialsFor(const QUrl &url) override { return std::vector<WebCredentials>(); }
+
+    void removeCredentials(const WebCredentials &credentials) override {}
+
+    void updateCredentials(const WebCredentials &credentials) override {}
+};
 
 #endif
 
