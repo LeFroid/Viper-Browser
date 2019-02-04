6b790145e42f0d1fbd26dcc2a2a0567203cb2fec
Timothy Vaccarelli <tmvfroid@gmail.com>
2019-02-04 18:19:13 -0500

    Created a service locator class. Will act as a registry for systems that are used throughout many parts of the browser (services like favicon store, bookmark manager, etc)

---
+++ src/core/utility/ServiceLocator.h
@@ -0,0 +1,80 @@
+#ifndef SERVICELOCATOR_H
+#define SERVICELOCATOR_H
+
+#include <unordered_map>
+
+/**
+ * @class ServiceLocator
+ * @brief Handles the registration and lookup of unique services.
+ *        This class does not own any of the services stored in its registry.
+ *        For QObject-derived types, this would be instantiated with
+ *        KeyType = QString, BaseServiceType = QObject
+ */
+template <class KeyType, class BaseServiceType>
+class ServiceLocator
+{
+private:
+    /// Non-copyable
+    ServiceLocator(const ServiceLocator&) = delete;
+
+    /// Non-copyable
+    ServiceLocator &operator=(const ServiceLocator&) = delete;
+
+public:
+    /// Default constructor
+    ServiceLocator() = default;
+
+    /// Default destructor
+    ~ServiceLocator();
+
+    /**
+     * @brief addService Attempts to add the given service to the registry.
+     * @param key Unique identifier of the service
+     * @param service Pointer to the service.
+     * @return
+     */
+    bool addService(const KeyType &key, BaseServiceType *service)
+    {
+        auto it = m_serviceMap.find(key);
+        if (it != m_serviceMap.end())
+            return false;
+
+        m_serviceMap[key] = service;
+        return true;
+    }
+
+    /**
+     * @brief getService Looks for and attempts to return a service with the given identifier.
+     * @param key Unique identifier of the service
+     * @return Pointer to the service if found, or a nullptr if not found
+     */
+    BaseServiceType *getService(const KeyType &key)
+    {
+        auto it = m_serviceMap.find(key);
+        if (it == m_serviceMap.end())
+            return nullptr;
+
+        return it->second;
+    }
+
+    /**
+     * @brief getServiceAs Looks for and attempts to return a service with the given identifier.
+     * @param key Unique identifier of the service
+     * @return Pointer to the service if found, or a nullptr if not found
+     */
+    template <class Derived>
+    Derived *getServiceAs(const KeyType &key)
+    {
+        static_assert(std::is_base_of<BaseServiceType, Derived>::value, "Object should inherit from BaseServiceType");
+
+        if (BaseServiceType *service = getService(key))
+            return dynamic_cast<Derived>(service);
+
+        return nullptr;
+    }
+
+private:
+    std::unordered_map<KeyType, BaseServiceType> m_serviceMap;
+};
+
+#endif // SERVICELOCATOR_H
