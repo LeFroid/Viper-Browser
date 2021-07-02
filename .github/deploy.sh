9d8fe5ae150648d9ed82fb04181495ad22eb7987
Timothy Vaccarelli <tmvfroid@gmail.com>
2021-07-02 17:55:53 -0400

    Start migration to github workflows

---
+++ .github/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd $GITHUB_WORKSPACE
+export VERSION=$(git rev-list master --count)
+cd $GITHUB_WORKSPACE/build
+wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/5/linuxdeployqt-5-x86_64.AppImage"
+chmod a+x linuxdeployqt-5-x86_64.AppImage
+./linuxdeployqt-5-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage -exclude-libs="libnss3.so,libnssutil3.so"
+find appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
+wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh
+bash upload.sh Viper*.AppImage*
---
+++ .github/setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo add-apt-repository ppa:beineri/opt-qt-5.15.2-focal -y
+sudo apt-get update -qq
+sudo apt-get install -qq qt515base qt515connectivity qt515multimedia qt515svg qt515translations qt515tools qt515webchannel qt515webengine
+source /opt/qt515/bin/qt515-env.sh
+export QTHOME=/opt/qt515
+export PATH=/opt/cmake/bin:/opt/qt515/bin:${PATH}
+export CMAKE_PREFIX_PATH=/opt/qt515:${CMAKE_PREFIX_PATH}
